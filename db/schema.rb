b0710f48e4a4225c4ad04fd0aaa52442f2a3cc4c
nusound <uroshni77@gmail.com>
2017-11-06 19:25:21 +0100

    Added navigation and created association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106135113) do
+ActiveRecord::Schema.define(version: 20171106182238) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171106135113) do
     t.string "director"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
