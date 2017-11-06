e8f7383549c2a85aa98eb801e3560b2ef0c3958b
nusound <uroshni77@gmail.com>
2017-11-06 14:06:45 +0100

    Created movies index and show actions and view pages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,35 @@
 class MoviesController < ApplicationController
+  before_action :find_movie, only:[:show, :edit, :update, :delete]
 
   def index
-
+    @movies = Movie.all.order('created_at DESC')
   end
+
+  def show
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :director)
+  end
+
+  def find_movie
+    @movie = Movie.find(params[:id])
+  end
+
 end
