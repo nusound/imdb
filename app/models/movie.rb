dfd960c4ce8e3a1bff1b9c19e1e3597be56d0b54
nusound <uroshni77@gmail.com>
2017-11-07 11:45:04 +0100

    Added images to index and show page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,6 @@ class Movie < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  has_attached_file :movie_img, :styles => { :movie_index => "250x350>", :movie_show => "325x475>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :movie_img, :content_type => /\Aimage\/.*\z/
 end
